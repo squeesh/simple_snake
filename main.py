b21c52ea832f642e194b3fe3a398740a96ea0882
squeesh <squeesh@gmail.com>
2016-02-15 20:56:30 -0500

    Added better walls

--- main.py
+++ main.py
@@ -11,11 +11,19 @@ BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+SQUARE_SCALE = 8*SCALE # size of squares
+
+def gen_walls():
+    outer_walls = [(i, 0) for i in range(1, 99)] + [(i, 99) for i in range(1, 99)] + \
+        [(0, i) for i in range(1, 99)] + [(99, i) for i in range(1, 99)]
+    inner_walls = [(i, 48) for i in range(48, 52)] + [(i, 51) for i in range(48, 52)] + \
+        [(48, i) for i in range(49, 51)] + [(51, i) for i in range(49, 51)]
+    return tuple(outer_walls + inner_walls)
+
+WALLS = gen_walls()
 
 screen = pygame.display.set_mode(SIZE)
-
 dir_buffer = deque(['r'])
-seg_offset = 8*SCALE # size of snake segments
 snake_segs = [(10, 10)]
 snake_length = 1
 
@@ -58,7 +66,7 @@ def check_collision():
     global game_over
 
     collision = False
-    if snake_segs[0] in snake_segs[1:]:
+    if snake_segs[0] in snake_segs[1:] or snake_segs[0] in WALLS:
         collision = True
 
     if collision:
@@ -90,10 +98,17 @@ def move_snake():
 def render_snake():
     for i in range(0, snake_length):
         seg_x, seg_y = snake_segs[i]
-        pygame.draw.rect(screen, GREEN, [seg_x*seg_offset, seg_y*seg_offset, seg_offset, seg_offset])
+        pygame.draw.rect(screen, GREEN, [seg_x*SQUARE_SCALE, seg_y*SQUARE_SCALE, SQUARE_SCALE, SQUARE_SCALE])
+
+
+def render_walls():
+    for i in range(0, len(WALLS)):
+        wall_x, wall_y = WALLS[i]
+        pygame.draw.rect(screen, BLUE, [wall_x*SQUARE_SCALE, wall_y*SQUARE_SCALE, SQUARE_SCALE, SQUARE_SCALE])
 
 
 def render_game_area():
+    render_walls()
     render_snake()
 
 
